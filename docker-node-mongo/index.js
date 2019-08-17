8df2b9a6277852c1ec9b7469daa51379359e4940
Dmytro9 <dpoliuliuk@gmail.com>
2019-08-17 22:50:13 +0300

    notes

--- docker-node-mongo/index.js
+++ docker-node-mongo/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Connect to MongoDB
 mongoose
   .connect(
-    'mongodb://mongo:27017/docker-node-mongo',
+    'mongodb://localhost:27017/myapp',
     { useNewUrlParser: true }
   )
   .then(() => console.log('MongoDB Connected'))
